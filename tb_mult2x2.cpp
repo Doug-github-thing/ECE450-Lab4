179c8e1f5aad507fb3379ba8c7820e23dd95981d
Doug-github-thing <dougrynar@gmail.com>
2025-10-28 22:57:29 -0400

    verifies output against expected

--- tb_mult2x2.cpp
+++ tb_mult2x2.cpp
@@ -7,6 +7,7 @@ int main() {
 	// Define initial test input
 	int a[2][2] = { {1, 3}, {2, 4} };
 	int b[2][2] = { {5, 7}, {6, 8} };
+	int expected[2][2] = { {23, 31}, {34, 46} };
 	int out[2][2];
 
 	// Run it through the filter
@@ -15,6 +16,11 @@ int main() {
 	// Print the results
 	for (int i=0; i<2; ++i)
 		printf("[%d, %d]\n", out[i][0], out[i][1]);
+
+	for (int i=0; i<2; ++i)
+		for (int j=0; j<2; ++j)
+			if (out[i][j] != expected[i][j])
+				return -1;
 
 	return 0;
 }
